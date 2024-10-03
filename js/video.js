cb599c88f4bc474de2d66ea3e8544227d290a8ef
shihabmia00 <shehabuddininfo00@gmail.com>
2024-10-03 14:38:06 +0600

    third commit

--- js/video.js
+++ js/video.js
@@ -30,24 +30,24 @@ const loadVideos = () => {
 }
 // Video Objects
 
-const videoDemo = {
-    category_id: "1003",
-    video_id: "aaac",
-    thumbnail: "https://i.ibb.co/NTncwqH/luahg-at-pain.jpg",
-    title: "Laugh at My Pain",
-    authors: [
-        {
-            "profile_picture": "https://i.ibb.co/XVHM7NP/kevin.jpg",
-            "profile_name": "Kevin Hart",
-            "verified": false
-        }
-    ],
-    others: {
-        "views": "1.1K",
-        "posted_date": "13885"
-    },
-    description: "Comedian Kevin Hart brings his unique brand of humor to life in 'Laugh at My Pain.' With 1.1K views, this show offers a hilarious and candid look into Kevin's personal stories, struggles, and triumphs. It's a laugh-out-loud experience filled with sharp wit, clever insights, and a relatable charm that keeps audiences coming back for more."
-}
+// const videoDemo = {
+//     category_id: "1003",
+//     video_id: "aaac",
+//     thumbnail: "https://i.ibb.co/NTncwqH/luahg-at-pain.jpg",
+//     title: "Laugh at My Pain",
+//     authors: [
+//         {
+//             "profile_picture": "https://i.ibb.co/XVHM7NP/kevin.jpg",
+//             "profile_name": "Kevin Hart",
+//             "verified": false
+//         }
+//     ],
+//     others: {
+//         "views": "1.1K",
+//         "posted_date": "13885"
+//     },
+//     description: "Comedian Kevin Hart brings his unique brand of humor to life in 'Laugh at My Pain.' With 1.1K views, this show offers a hilarious and candid look into Kevin's personal stories, struggles, and triumphs. It's a laugh-out-loud experience filled with sharp wit, clever insights, and a relatable charm that keeps audiences coming back for more."
+// }
 
 
 
@@ -57,17 +57,29 @@ const loadVideoDisplay = (videos) => {
         const divContainer = document.createElement('div')
         divContainer.classList = 'card card-compact'
         divContainer.innerHTML = `
-        <figure>
-    <img
+        <figure class="h-[200px] relative">
+    <img class="h-[90%] object-cover rounded-xl"
       src="${video.thumbnail}"
       alt="" />
+    ${video.others.posted_date?.length == 0 ? "" : `<span class="absolute right-2 bottom-3 bg-black rounded-xl p-1 text-white">${video.others.posted_date}</span>` }
+
+      
   </figure>
-  <div class="card-body">
-    <h2 class="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div class="card-actions justify-end">
-      <button class="btn btn-primary">Buy Now</button>
+  <div class="p-0 py-2 flex item-center gap-3">
+    <div class="w-8">
+    <img class="w-full object-cover rounded-full" src=${video.authors[0].profile_picture} >
+    
     </div>
+
+    <div class="font-bold ">
+    <h2>${video.title}</h2>
+    <div class= "flex item-center gap-2"> <p class="text-gray-600 font-normal text-[14px]">${video.authors[0].profile_name}</p> 
+    
+    ${video.authors[0].verified == true ? `<img class="w-4 " src="https://img.icons8.com/?size=100&id=D9RtvkuOe31p&format=png&color=000000">` : ''}
+    </div>
+    
+    </div>
+    
   </div>
         `
         videosContainer.appendChild(divContainer)
